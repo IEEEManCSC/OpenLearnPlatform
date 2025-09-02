51fbdf7189f694d88acff80d695188b189eb0ef6
Seif El-Din Sweilam <saifsweelam@gmail.com>
2025-09-03 01:33:56 +0300

    feat(api): apply cors configuration

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,5 +1,6 @@
 import "express-async-errors"; // For Express 4.x Error Handling
 import express from "express";
+import cors from "cors";
 import { auth } from "./lib/auth.js";
 import { toNodeHandler } from "better-auth/node";
 import { admin, adminRouter } from "./lib/admin.js";
@@ -8,6 +9,13 @@ import { addEnhancedSendMethod } from "./middlewares/enhanced-send.js";
 
 const app = express();
 app.disable("x-powered-by");
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://olp.csc.ieeemansb.org"],
+    credentials: true,
+  }),
+);
 
 app.all("/api/auth/*", toNodeHandler(auth));
 
